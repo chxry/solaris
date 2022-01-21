14cdef07985359651eead2fe5eec2f57a7e49a95
chxry <moosejuiceTex@gmail.com>
2022-01-21 15:43:29 +0000

    jp translations

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -3,12 +3,8 @@ import { useTranslation } from "react-i18next";
 import { TranslateIcon, InformationCircleIcon } from "@heroicons/react/solid";
 import { CircleFlag } from "react-circle-flags";
 
+import { LANGS } from "../locale";
 import { NAME, VERSION } from "../config";
-
-const langs = {
-  gb: "English",
-  ru: "Русский",
-};
 
 const Bar = () => {
   const { t, i18n } = useTranslation();
@@ -23,20 +19,21 @@ const Bar = () => {
         <span className="flex ml-auto relative">
           {localeOpen && (
             <div className="absolute right-7 w-max top-7 bg-polar-0 rounded-md drop-shadow font-md">
-              {Object.keys(langs).map((lang) => (
+              {Object.keys(LANGS).map((lang) => (
                 <button
                   className={
-                    "flex items-center text-left p-1.5 transition-colors hover:bg-frost-2 rounded-md" +
+                    "flex items-center text-left p-1.5 transition-colors hover:bg-frost-2 rounded-md w-full" +
                     (i18n.language === lang ? " bg-polar-1" : "")
                   }
                   onClick={() => {
                     i18n.changeLanguage(lang);
+                    localStorage.setItem("lang", lang);
                     setLocaleOpen(false);
                   }}
                   key={lang}
                 >
                   <CircleFlag countryCode={lang} className="h-5 mr-2" />
-                  {langs[lang]}
+                  {LANGS[lang]}
                 </button>
               ))}
             </div>
--- src/locale/index.ts
+++ src/locale/index.ts
@@ -3,14 +3,26 @@ import { initReactI18next } from "react-i18next";
 
 import gb from "./gb.json";
 import ru from "./ru.json";
+import jp from "./jp.json";
 
+export const LANGS = {
+  gb: "English",
+  ru: "Русский",
+  jp: "日本語",
+};
+
+let lang = localStorage.getItem("lang");
+if (!lang) {
+  localStorage.setItem("lang", "gb");
+  lang = "gb";
+}
 i18n.use(initReactI18next).init({
-  lng: "gb",
+  lng: lang,
   debug: process.env.NODE_ENV !== "production",
   interpolation: {
     escapeValue: false,
   },
-  resources: { gb, ru },
+  resources: { gb, ru, jp },
 });
 
 export default i18n;
