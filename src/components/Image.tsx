c4c2b0d631f4a87a1a71bb47be1477ca69f86c4d
chxry <moosejuiceTex@gmail.com>
2022-01-22 19:08:52 +0000

    fix rounding

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -17,7 +17,7 @@ const Image = ({
   return (
     <div className="relative mx-12 my-6">
       <img src={src} alt={alt} className="md:h-100 rounded-md" />
-      <p className="absolute bottom-0 text-sm opacity-40 bg-black rounded-tr-xl rounded-bl-xl p-0.5">
+      <p className="absolute bottom-0 text-sm opacity-40 bg-black rounded-tr-md rounded-bl-md p-0.5">
         {t("attribute photo", {
           author,
           platform,
