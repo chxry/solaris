0e9c3508a2e36dbc0884adb633f224b6b47a052a
chxry <moosejuiceTex@gmail.com>
2022-02-02 18:18:30 +0000

    switch hashrouter to browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Bar } from "./components";
 import { Planner, NotFound, Home } from "./pages";
@@ -10,14 +10,14 @@ import "./locale";
 const App = () => {
   return (
     <main className="w-screen min-h-screen flex flex-col bg-polar-1 text-snow-0 font-body">
-      <HashRouter>
+      <BrowserRouter>
         <Bar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/planner" element={<Planner />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </main>
   );
 };
